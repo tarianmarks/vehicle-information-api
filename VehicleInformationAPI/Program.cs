b17975a8881d50062464e9d95243df83bf31fc74
tarian.marks@wonkum.com <tarian.marks@wonkum.com>
2025-06-10 09:49:38 -0400

    Fixed swagger.  There needed to be a change in the url of the browser.

--- VehicleInformationAPI/Program.cs
+++ VehicleInformationAPI/Program.cs
@@ -2,6 +2,7 @@
 //using Microsoft.EntityFrameworkCore.Design;
 using VehicleInformationAPI.BusinessLayer;
 using VehicleInformationAPI.BusinessLayer.Interfaces;
+using VehicleInformationAPI.DataLayer.Repositories;
 //using VehicleInformationAPI.DataLayer;
 //using VehicleInformationAPI.DataLayer.Interfaces;
 //using VehicleInformationAPI.DataLayer.Repositories;
@@ -16,8 +17,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IVehicleInformationService, VehicleInformationService>();
+builder.Services.AddScoped<IVehicleInformationRepository, VehicleInformationRepository>();
 //builder.Services.AddScoped<IClient, Client>();
-//builder.Services.AddScoped<IAttorneyRepository, AttorneyRepository>();
 //builder.Services.AddScoped<DbContext, DataContext>();
 //builder.Services.AddDbContext<DataContext>(/*options =>
 //    options.UseSqlServer(connectionString)*/);
@@ -30,7 +31,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
