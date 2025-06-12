5d6aaf6d051f156d50bad55043f7d3e5011d9c54
tarian.marks@wonkum.com <tarian.marks@wonkum.com>
2025-06-12 03:48:11 -0400

    Clean-up.  Removing pword from appsettings

--- VehicleInformationAPI/Program.cs
+++ VehicleInformationAPI/Program.cs
@@ -1,17 +1,10 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using AutoMapper;
 using VehicleInformationAPI.BusinessLayer;
 using VehicleInformationAPI.BusinessLayer.Interfaces;
 using VehicleInformationAPI.DataLayer;
 
-//using VehicleInformationAPI.DataLayer;
 using VehicleInformationAPI.DataLayer.Interfaces;
 using VehicleInformationAPI.DataLayer.Repositories;
-using Microsoft.Extensions.Configuration;
-using System.Configuration;
-using VehicleInformationAPI;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,35 +14,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(MapperProfile));
-//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-//builder.Services.AddAutoMapper(new[] { typeof(VehicleInformationAPI.BusinessLayer).Assembly });
 
-//builder.Services.AddDbContext<DataContext>();
 builder.Services.AddScoped<IVehicleInformationService, VehicleInformationService>();
 builder.Services.AddScoped<IVehicleInformationRepository, VehicleInformationRepository>();
-//builder.Services.AddScoped<DbContext, DataContext>();
 builder.Services.AddScoped<IReadFromCsv, ReadFromCsv>();
 builder.Services.AddScoped<IMyMapper, MyMapper>();
 builder.Services.AddHttpClient();
-//builder.Services.AddScoped<IClient, Client>();
-//builder.Services.AddScoped<DbContext, DataContext>();
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("VehicleInformationDb")));
 
-//Automapper config
-//var configuration = new MapperConfiguration(cfg =>
-//{
-//    //cfg.AddProfile(Mapper);
-//    //cfg.AddMaps(new[]
-//    //{
-//    //    typeof(VehicleInformationService),
-//    //});
-
-//    //cfg.AddProfile<MapperProfile>();
-//    //cfg.AddMaps(typeof(VehicleInformationService));
-//});
-//IMapper mapper = configuration.CreateMapper();
-//builder.Services.AddSingleton(mapper);
 
 var app = builder.Build();
 
